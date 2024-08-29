35e16b9d4f9e0dcd3b8b1c61ebcf367242c51469
Tiago S. Peixoto <tspxttech@gmail.com>
2024-08-29 16:49:04 -0300

    alteração funcionalidade carrinho
    alterada a pagina html do carrinho e agora os dados do pedido são exibidos na pagina

--- ecommerce/loja/views.py
+++ ecommerce/loja/views.py
@@ -47,7 +47,12 @@ def adicionar_carrinho(request, id_produto):
         return redirect('loja')
 
 def carrinho(request):
-    return render(request, 'carrinho.html')
+    if request.user.is_authenticated:
+        cliente = request.user.cliente
+    pedido, criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+    itens_pedido = ItensPedido.objects.filter(pedido=pedido)
+    context = {"itens_pedido": itens_pedido, "pedido": pedido}
+    return render(request, 'carrinho.html', context)
 
 def checkout(request):
     return render(request, 'checkout.html')
